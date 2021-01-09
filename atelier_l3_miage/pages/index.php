42ebfb76fb2f54f7b8b19656cec7d8edbe553bea
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-01-09 15:12:19 +0100

    added the VM and the source code

---
+++ atelier_l3_miage/pages/index.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello world PHP</p>'; 
+
+// $conn = new mysqli('mysql-container', 'devops-user', 'devops-user-pass');
+
+// if ($conn->connect_error) {
+  // die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+
+?>
+</body>
+</html>
